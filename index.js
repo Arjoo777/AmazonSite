d29f35b7e22af985df111fa743b57d4cccf30cc0
Arjoo Shaikh <arjooshaikh35@gmail.com>
2022-09-11 17:04:36 +0530

    coupon_code

--- index.js
+++ index.js
@@ -70,7 +70,18 @@ const changeMode=()=>{
     mybody.classList.toggle('mydark')
 }
 
-$(function(){
-    $('.selectpicker').selectpicker();
-});
+
+// $(function(){
+//     $('.selectpicker').selectpicker();
+// });
+function loadCoupon(){
+    document.getElementById('coupon').style.display="block";
+    document.getElementById('search').style.opacity="0.5";
+}
+
+const closeCoupon = () => {
+    document.getElementById('coupon').style.display="none";
+    document.getElementById('search').style.opacity="1";
+}
+
 
